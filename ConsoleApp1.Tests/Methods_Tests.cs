25e29f04e11ef995684d3e0872861fdf95519305
Vira Murray <vira.avramenko@gmail.com>
2022-10-05 10:02:58 -0400

    TDD-console-app-sample

---
+++ ConsoleApp1.Tests/Methods_Tests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp1_Tests;
+
+internal class Methods_Tests
+{
+    [Theory]
+    [InlineData(1,1,2)]
+    [InlineData(-1, -1, -2)]
+    [InlineData(1, -1, 0)]
+    [InlineData(0, 0, 0)]
+    public void Add(int num1, int num2, int expectedSum)
+    {
+        //Arrange: This is where we prepare the code in order to call the method you need to test
+        var m = new Methods_Tests();
+        //For example, we may need to instantiate an instance of the class that will contain the code we are testing
+        //Act: This is where we actually call the method we want to test
+        var actual = m.Add(num1, num2);
+        //Simply put, we need to call what we want to test
+        //Assert: Check against a constant(check against what is expected)
+        //Verify that the code we wrote behaves as expected
+        Assert.Equal(expectedSum, actual);
+    }
+}
---
+++ ConsoleApp1/Methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Methods
+    {
+        private int myField = 10;
+        public int Add (int num1, int num2)
+        {
+            throw new NotImplementedException();
+            //return num1 + num2;
+        }
+        public int Substract(int num1, int num2)
+        {
+            //return num1 - num2;
+            throw new NotImplementedException();
+        }
+        public int Devide(int num1, int num2)
+        {
+            //return num1 / num2;
+            throw new NotImplementedException();
+        }
+        public int Multiply(int num1, int num2)
+        {
+            //return num1 * num2;
+            throw new NotImplementedException();
+        }
+        public int Modulo(int num1, int num2)
+        {
+            //return num1 % num2;
+            throw new NotImplementedException();
+        }
+    }
+}
